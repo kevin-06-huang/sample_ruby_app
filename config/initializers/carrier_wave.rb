f7bd559b1bbc29dc2883a5fbcc03473883eded5b
kevincrazykid <kevincrazykid@gmail.com>
2015-04-16 16:01:53 -0700

    fourth attempt to fix deployment ssl error by switching the name of the bucket to one without the period

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -6,7 +6,7 @@ if Rails.env.production?
       :aws_access_key_id => ENV['S3_ACCESS_KEY'],
       :aws_secret_access_key => ENV['S3_SECRET_KEY'],
       # we added the next part to account for the period in the name
-      :path_style => true
+    # :path_style => true
     }
     config.fog_directory = ENV['S3_BUCKET']
   end
