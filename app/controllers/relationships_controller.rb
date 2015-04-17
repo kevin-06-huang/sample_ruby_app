6a5e04afba521b9af54e3cb702f4f3720b8d8a9b
kevincrazykid <kevincrazykid@gmail.com>
2015-04-17 16:58:33 -0700

    fix ajax

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -6,20 +6,20 @@ class RelationshipsController < ApplicationController
   before_action :logged_in_user
   # both added from 12.32
   def create
-    user = User.find(params[:followed_id])
-    current_user.follow(user)
+    @user = User.find(params[:followed_id])
+    current_user.follow(@user)
     # modified in listing 12.35 to respond to ajax
     respond_to do |format|
-      format.html { redirect_to user }
+      format.html { redirect_to @user }
       format.js
-     end
+    end
   end
   def destroy
-    user = Relationship.find(params[:id]).followed
+    @user = Relationship.find(params[:id]).followed
     current_user.unfollow(user)
     # the next section respond to ajax
     respond_to do |format|
-      format.html { redirect_to user }
+      format.html { redirect_to @user }
       format.js
     end
   end
